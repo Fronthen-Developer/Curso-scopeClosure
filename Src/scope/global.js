19949340e4091f3b238b67f4e87fb080473a75e2
DeivyRivas <deyruiz555@gmail.com>
2024-01-04 14:29:30 -0500

    se agrega scope y archivo npm

---
+++ Src/scope/global.js
@@ -0,0 +1,14 @@
+// vamos a enender las variable y ver como fucnioes la asignancion y otros
+
+//variables
+var a; //declarando
+var b = 'b'; // declarams y asignamos
+b = 'bb' // reasignacion
+var a = 'aa' //redeclaracion
+
+// global Scope
+var fruit = 'Apple' // global 
+function bestfruit(){
+    console.log(fruit);
+}
+bestfruit();
